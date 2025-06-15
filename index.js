431d105f3db5d5b34ac4ec3f7abe33d4b08d9039
waweru-sdf <felixwaweru200@gmail.com>
2025-06-15 22:24:26 +0300

    index.js index.html index.css changes

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
+const input = document.getElementById('type');
+const button = document.getElementById('btn');
+const list = document.getElementById('itemList');
+const message = document.getElementById('message');
 
+
+// Add click event
+button.addEventListener('click', () => {
+  const inputValue = input.value.trim(); // Get input value
+
+ const currentGuests = list.getElementsByTagName('li').length;
+
+  if (currentGuests >= 10) {
+    message.textContent = "You can only add up to 10 guests.";
+    return;
+  }
+
+
+  if (inputValue !== "") {
+    const listItem = document.createElement('li'); // Create new list item
+    listItem.textContent = inputValue; // Set its text
+    list.appendChild(listItem); // Add it to the list
+    input.value = ""; // Clear input
+    message.textContent="";
+  }
+});
 
 
 
